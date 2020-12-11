0bff073ffd22aaff0fc7277db832b50ad86a866c
Avokoda <dakotamum@gmail.com>
2020-12-10 21:10:06 -0700

    day7 - got add bag functionality working

--- day7/main.cpp
+++ day7/main.cpp
@@ -2,17 +2,80 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
 using namespace std;
-int main(){
-    ifstream fin("input.txt");
-    if(!fin){cout << "File could not be opened..." << endl;}else{cout << "Successfully opened input!" << endl;}
-    string line;
 
-//***************************************** WHILE LOOP
-    while(getline(fin, line)){
+struct Bag {
+    int quantity;
+    string description;
+    vector<Bag*> children;
+};
+
+bool insertBag(Bag* bagptr, Bag aBag) {
+    Bag* tempBag = bagptr;
+    bool found = false;
+    cout << "current bag we're looking at: " << tempBag->description << endl;
+    if (tempBag->children.size() != 0) {
+        for (int i = 0; i < tempBag->children.size(); i++) {
+            if (insertBag(tempBag->children[i], aBag)) { found = true; }
+        }
+    }
+    if (tempBag->description == aBag.description) {
+
+        tempBag->children = aBag.children;
+        return true;
+    }
+    else {
+        return found;
+    }
+}
+
+void addBag(vector<Bag>& roots, Bag aBag) {
+    bool found = false;
+    for (int i = 0; i < roots.size(); i++) {
+        Bag* bagptr = &roots[i];
+        if (insertBag(bagptr, aBag)) { found = true; }
+    }
+    if (!found) {
+        roots.push_back(aBag);
+    }
+}
+
+void addBagToGoldContainers(vector<string>& bagsWithGoldBags, string aBag) {
+    bool found = false;
+    for (int i = 0; i < bagsWithGoldBags.size(); i++) {
+        if (bagsWithGoldBags[i] == aBag) {
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        bagsWithGoldBags.push_back(aBag);
+    }
+}
+
+bool searchBags() {
+    
+}
+
+void findNumGoldBags(vector<Bag>& rootBags, vector<string>& bagsWithGoldBags) {
+    for (int i = 0; i < rootBags.size(); i++) {
+        if (searchBags()) {
+            addBagToGoldContainers(bagsWithGoldBags, rootBags[i].description);
+        }
+    }
+}
+
+
+int main() {
+    ifstream fin("input.txt");
+    if (!fin) { cout << "File could not be opened..." << endl; }
+    else { cout << "Successfully opened input!" << endl; }
+    string line;
+    vector<Bag> rootBags;
+
+    while (getline(fin, line)) {
         size_t foundNo = line.find(" no ");
-        if(foundNo == string::npos){
+        if (foundNo == string::npos) {
             string parent = line.substr(0, line.find("bag") - 1);
             vector<int> quantities = {};
             vector<string> descriptions = {};
@@ -20,33 +83,38 @@ int main(){
             line.erase(0, line.find(" ") + 1);
             bool keepgoing = true;
 
-            while(keepgoing){
-            //cout << line << endl;
-            quantities.push_back(stoi(line.substr(0, line.find(" ")))); 
-            //cout << line.substr(0, line.find(" ")) << "***"<< endl;
-            line.erase(0, line.find(" ") + 1);
-            //cout << "line after erase:***" << line << endl;
-            descriptions.push_back(line.substr(0, line.find("bag") - 1));
-            //cout << "***" << line.substr(0, line.find("bag")) << endl;
-            line.erase(0, line.find("bag"));
-            line.erase(0, 2);
-            size_t foundBag = line.find("bag");
-            if(foundBag != string::npos){
+            while (keepgoing) {
+                quantities.push_back(stoi(line.substr(0, line.find(" "))));
                 line.erase(0, line.find(" ") + 1);
-            }
-            else{
-                break;
-            }
-            //cout << "line after erase:***" << line << endl;
+                descriptions.push_back(line.substr(0, line.find("bag") - 1));
+                line.erase(0, line.find("bag"));
+                line.erase(0, 2);
+                size_t foundBag = line.find("bag");
+                if (foundBag != string::npos) {
+                    line.erase(0, line.find(" ") + 1);
+                }
+                else {
+                    break;
+                }
             }
 
-            cout << "parent:***" << parent << "******" << endl;
-            for(int i = 0; i < descriptions.size(); i++){
-                cout << "***" << quantities[i] << "***" << descriptions[i] << "***" << endl;
+            Bag aBag;
+            aBag.description = parent;
+            aBag.quantity = 1;
+            for (int i = 0; i < quantities.size(); i++) {
+                Bag *childBag = new Bag;
+                childBag->quantity = quantities[i];
+                childBag->description = descriptions[i];
+                aBag.children.push_back(childBag);
             }
+            addBag(rootBags, aBag);
         }
     }
-//****************************************************** WHILE LOOP
+
+    vector<string> bagsWithGoldBags = {};
+    findNumGoldBags(rootBags, bagsWithGoldBags);
+
+    cout << rootBags[1].description << "->" << rootBags[1].children[0]->description << endl;
 
     fin.close();
     return 0;
