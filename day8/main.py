8a71ccdf76b9c89ed8c79e99f3a08519773eb0dd
Avokoda <dakotamum@gmail.com>
2020-12-12 17:32:27 -0700

    completed day8 part a

--- day8/main.py
+++ day8/main.py
@@ -11,17 +11,13 @@ accumulation = 0
 if len(steps) != 0:
     currentLine = 0
     while revisited == 'false':
-        #print("Current line is: " + str(currentLine))
         nextCurrentLine = currentLine + 1
         if steps[currentLine][2] == 'true':
             revisited = 'true'
-            #print(str(currentLine) + " was already visited!")
             break
         elif steps[currentLine][0] == 'acc':
             accumulation += int(steps[currentLine][1])
-            #print("Adding " + str(steps[currentLine][1]))
         elif steps[currentLine][0] == 'jmp':
-            #print("Jumping to line " + str(nextCurrentLine + int(steps[currentLine][1])))
             nextCurrentLine += int(steps[currentLine][1]) - 1
         steps[currentLine][2] = 'true'
         currentLine = nextCurrentLine
