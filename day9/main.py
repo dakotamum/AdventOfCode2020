8298ab835184530b930ef33d4cb04ef5efd6ce41
Avokoda <dakotamum@gmail.com>
2020-12-13 10:33:48 -0700

    completed day 9 part a

--- day9/main.py
+++ day9/main.py
@@ -15,19 +15,30 @@ numbers = []
 for line in file:
     numbers.append(int(line))
 
-print(numbers)
+#print(numbers)
 
-preamble = 5
-
+preamble = 25
 currentNumberArray = []
 
-for currentNumberIndex in range(5):
+for currentNumberIndex in range(preamble):
     currentNumberArray.append(numbers[currentNumberIndex])
 
-print("current number array: ")
-print(currentNumberArray)
+#print("current number array: ")
+#print(currentNumberArray)
 
-for i in range(preamble, len(numbers)):
-    print(numbers[i])
+#for i in range(preamble, len(numbers)):
+#    print(numbers[i])
 
-print(validateNumber(numbers[preamble], numbers))
+#print(validateNumber(numbers[preamble], numbers))
+
+numberThatDoesntAddUp = 0
+
+for iter in range(preamble, len(numbers)):
+    if validateNumber(numbers[iter], currentNumberArray) == 'false':
+        numberThatDoesntAddUp = numbers[iter]
+        break
+    else:
+        currentNumberArray.pop(0)
+        currentNumberArray.append(numbers[iter])
+
+print("Number that doesn't add up: " + str(numberThatDoesntAddUp))
